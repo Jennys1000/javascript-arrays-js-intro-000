7f2b7890d82928b5451d57e188dcb4ce79e3a649
Jennifer Sanders <jennys1000@gmail.com>
2020-12-07 01:12:58 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 function addElementToBeginningOfArray(chocolateBars) {'snickers,' 'hundred grand', 'kitkat', 'skittles '
-return [reeses, ... chocolateBars]
+return [reeses,...chocolateBars]
 }
+
