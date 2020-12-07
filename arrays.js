c2ca0c66d2f22b344e7c1a6cc0e4f18ef1c06e19
Jennifer Sanders <jennys1000@gmail.com>
2020-12-07 02:19:33 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,3 +22,6 @@ function destructivelyRemoveElementFromBeginningOfArray(chocolateBars) {
   chocolateBars.shift()
   return chocolateBars
 }
+function removeElementFromBeginningOfArray(chocolateBars) { 
+  return chocolateBars.slice(1)
+}
